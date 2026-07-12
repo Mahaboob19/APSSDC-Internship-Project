adfd087f05341040803523599ad8b611fd1dd495
Mahaboob <mspattan9@email.com>
2026-07-12 11:41:39 +0530

    s3 frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,8 +17,7 @@ app.use(helmet());
 
 app.use(cors({
   origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  credentials: true
 }));
 
 // Parses JSON request bodies (for POST/PUT). A size limit is set to reduce
